21230b1cbfce1fab8d460ac551847994e416a5ff
ER2-1017 <60078003+ER2-1017@users.noreply.github.com>
2020-07-27 02:21:30 +0300

    Add files via upload

---
+++ server.java
@@ -0,0 +1,46 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+public class Server {
+    private void run() throws Exception {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(mainGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(
+                                    //максимальный размер объекта, который мы можем прочитать 100 Мб:
+                                    new ObjectDecoder(100 * 1024 * 1024, ClassResolvers.cacheDisabled(null)), //ловит набор байтов и восстанавливает из них объект
+                                    new ObjectEncoder(), //получает объект и делает из него набор байт
+                                    new AuthHandler()
+                            );
+                        }
+                    })
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture future = bootstrap.bind(8190).sync();
+            DataBaseService.connect();
+            System.out.println("Сервер подключился");
+            future.channel().closeFuture().sync();
+        } finally {
+            mainGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+            DataBaseService.disconnect();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Server().run();
+    }
+}
